ee60df5d5b04948a7919ef88680da108f0c2c812
Mel Koppens <mkoppens12@hotmail.com>
2024-04-21 04:23:08 -0700

    turns, obs and hdg working

---
+++ src/components/Frame.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Compass from './Compass'
+
+export default function Frame() {
+  
+  return (
+    <main className="framework">
+      <Compass />
+      <img id="outer-frame" src="src/assets/Outer-Framwork.svg" alt="outer-frame" />
+    </main>
+  )
+}
