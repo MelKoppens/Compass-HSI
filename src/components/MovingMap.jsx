95d86fb3d57282a96a3b4bf25358404a2b071c83
Mel Koppens <mkoppens12@hotmail.com>
2024-04-22 20:05:27 -0700

    CDI is now controllable

---
+++ src/components/MovingMap.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MovingMap() {
+  return (
+    <main className="movingmap">
+      <h2>I will be a Moving Map Display</h2>
+      <p><img id="under-construction" src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Under_construction.JPG" alt="outer-frame" /></p>
+    </main>
+  
+  )
+}
