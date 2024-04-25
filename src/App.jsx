acd0fd00f47f55b08202ba34e94592cf1a78ce9b
Mel Koppens <mkoppens12@hotmail.com>
2024-04-24 19:09:10 -0700

    moving map working properly

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,15 @@ import MovingMap from './components/MovingMap'
 import './App.css'
 
 let FPS = 10; // determines how fast states are updated 
-let TURN_RATE = 6; // determine the turn rate (3 deg/sec is standard in aviation)
-let SPEED = 240; // in KTS
+let TURN_RATE = 3; // determine the turn rate (3 deg/sec is standard in aviation)
+let SPEED = 120; // in KTS
 
 function App() {
-
-  const [headingState, setHeadingState] = useState(0);
+  const [aircraftState, setAircraftState] = useState({heading: 0, xPos: 0, yPos: 0});
   const [obsState, setObsState] = useState(30);
   const [cdiState, setCdiState] = useState(0);
   const [bugState, setBugState] = useState(0);
   const [turnState, setTurnState] = useState('right'); // 'left' , 'right' , 'level
-  const [xPosState, setXPosState] = useState(0);
-  const [yPosState, setYPosState] = useState(0);
 
   // handle left turn
   const handleTurnLeft = () => {
@@ -113,11 +110,9 @@ function App() {
   };
 
   // module to test states
-  useEffect(() => {
-    console.log('xPos: ' + xPosState);
-    console.log('yPos: ' + yPosState);
-    console.log('heading: ' + headingState);
-  }, [headingState, xPosState, yPosState]);
+  // useEffect(() => {
+ 
+  // }, [aircraftState]);
 
   // Game loop for updating opponent positions, collisions, etc.
   useEffect(() => {
@@ -125,32 +120,42 @@ function App() {
     const gameLoop = setInterval(() => {
       // Update opponent positions, handle collisions, etc.
 
-      // Handle turns when turning 
-      setHeadingState((prevHeading) => {
+      // Handle aircraft movement
+      setAircraftState((prevAircraftState) => {
+        // console.log(prevAircraftState);
         let newHeading;
         if (turnState === 'left') {
-          newHeading = prevHeading - (TURN_RATE / FPS); // 3 refers to std rate of turn (3deg per sec)
+          newHeading = prevAircraftState.heading - (TURN_RATE / FPS); // 3 refers to std rate of turn (3deg per sec)
         } else if (turnState === 'right') {
-          newHeading = prevHeading + (TURN_RATE / FPS);
+          newHeading = prevAircraftState.heading + (TURN_RATE / FPS);
         } else {
-          newHeading = prevHeading;
+          newHeading = prevAircraftState.heading;
         }
         comp.style.transform = `rotate(${-newHeading}deg)`;
-        return newHeading;
+
+        let newXPos = prevAircraftState.xPos + Math.sin(newHeading * Math.PI / 180) * SPEED / ( FPS * 36 );
+        let newYPos = prevAircraftState.yPos - Math.cos(newHeading * Math.PI / 180) * SPEED / ( FPS * 36 );
+
+        let newAircraftState = {
+          heading: newHeading,
+          xPos: newXPos,
+          yPos: newYPos
+        }
+        return newAircraftState;
       });
 
       // Handle forward movement
-      setXPosState((prevXPos) => {
-        let newXPos;  
-        newXPos = prevXPos + Math.sin(headingState * Math.PI / 180) * SPEED / ( FPS * 36 );
-        return newXPos;
-      });
+      // setXPosState((prevXPos) => {
+      //   let newXPos;  
+      //   newXPos = prevXPos + Math.sin(headingState * Math.PI / 180) * SPEED / ( FPS * 36 );
+      //   return newXPos;
+      // });
       
-      setYPosState((prevYPos) => {
-        let newYPos;
-        newYPos = prevYPos - Math.cos(headingState * Math.PI / 180) * SPEED / ( FPS * 36 );
-        return newYPos;
-      });
+      // setYPosState((prevYPos) => {
+      //   let newYPos;
+      //   newYPos = prevYPos - Math.cos(headingState * Math.PI / 180) * SPEED / ( FPS * 36 );
+      //   return newYPos;
+      // });
 
     }, 1000 / FPS);
     return () => clearInterval(gameLoop);
@@ -173,9 +178,7 @@ function App() {
       />
       <Frame />
       <MovingMap 
-        xPosState = {xPosState}
-        yPosState = {yPosState}
-        headingState = {headingState}
+        aircraftState = {aircraftState}
       />
     </div>
   )
--- src/components/MovingMap.jsx
+++ src/components/MovingMap.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import Map from  './Map'
 export default function MovingMap(props) {
-  const { 
-    xPosState,
-    yPosState,
-    headingState
-  } = props;
-  // console.log(xPosState + ' xPos in MM');
-  // console.log(yPosState + ' yPos in MM');
-  // console.log(headingState + ' hdg in MM');
+  const { aircraftState } = props;
+
   return (
     <main className="movingmap">
-      <Map
-        xPosState = {xPosState}
-        yPosState = {yPosState}
-        headingState = {headingState} 
+      <Map className="map"
+        aircraftState = {aircraftState}
       />
+      <img src="src/assets/miniplane.svg" alt="Miniplane" className="miniplane" width = "40"/>
     </main>
   
   )
