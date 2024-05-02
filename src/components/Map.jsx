144200737dc54f43740f0a8cdee4ee53634aa520
Mel Koppens <mkoppens12@hotmail.com>
2024-05-01 22:58:09 -0700

    used vite build

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,12 @@
-import '../App.css'
-
+import '../App.css';
+import Testmap from '../assets/Testmap.svg';
 export default function Map(props) {
   const { aircraftState } = props;
 
   return (
     <div>
       <svg className="map" width="520" height="500" viewBox={`${aircraftState.xPos + 290} ${aircraftState.yPos + 300} 520 500`} xmlns="http://www.w3.org/2000/svg">
-        <image href="src/assets/Testmap.svg" width="1100" height="1100" transform={`rotate(${-aircraftState.heading} ${aircraftState.xPos + 550} ${aircraftState.yPos + 550})`}/>
+        <image href={Testmap} width="1100" height="1100" transform={`rotate(${-aircraftState.heading} ${aircraftState.xPos + 550} ${aircraftState.yPos + 550})`}/>
       </svg>
     </div>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import svgLoader from 'vite-svg-loader'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader()],
+  plugins: [react(), svgr()],
 })
