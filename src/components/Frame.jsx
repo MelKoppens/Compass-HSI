1a4ca6413981cf3c5b765eb2d29c359ac6e64ce1
Mel Koppens <mkoppens12@hotmail.com>
2024-05-01 19:44:09 -0700

    added heading/crs/bug text

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Compass from './Compass'
 import CompassFrame from './CompassFrame'
+import HeadingTxt from './HeadingTxt'
+import BugTxt from './BugTxt'
+import CourseTxt from './CourseTxt'
 
-export default function Frame() {
-  
+export default function Frame(props) {
+  const { aircraftState, bugState, obsState } = props;
   return (
     <main className="framework">
-      <Compass />
       <CompassFrame />
-      {/* <img id="outer-frame" src="src/assets/OuterFramwork.svg" alt="outer-frame" /> */}
+      <Compass />
+      <HeadingTxt aircraftState = {aircraftState}/>
+      <BugTxt bugState = {bugState}/>
+      <CourseTxt obsState = {obsState}/>
     </main>
   )
 }
--- src/components/MovingMap.jsx
+++ src/components/MovingMap.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Map from  './Map'
+import Radial from  './Radial'
 export default function MovingMap(props) {
-  const { aircraftState } = props;
+  const { aircraftState , instrumentState, obsState } = props;
 
   return (
     <main className="movingmap">
       <Map className="map"
         aircraftState = {aircraftState}
+      />
+      <Radial className="radial"
+        aircraftState = {aircraftState}
+        instrumentState = {instrumentState}
+        obsState = {obsState}
       />
       <img src="src/assets/miniplane.svg" alt="Miniplane" className="miniplane" width = "40"/>
     </main>
